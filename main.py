fa37972b3218333c702401064be1dc048a6ef0ac
Nelson <nelbalvin2005@gmail.com>
2026-03-11 15:48:55 -0500

    Agregada funcionalidad en el Main

---
+++ main.py
@@ -0,0 +1,15 @@
+#PROGRAM IMPUT
+from Sales_Calculation_Function import sales
+
+print("Welcome, please enter the dsta to register the sale")
+
+try:
+    item = input("Enter article Name: ")
+    price = float(input("Enter Item Price:" ))
+    amount = int(input("Quantity of Items Sold: "))
+
+    if price ==float and amount ==int:
+        sales(price, amount)
+        
+except:
+    print("Pleace enter numeric values in Price and Quality")
