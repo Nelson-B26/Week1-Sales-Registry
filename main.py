ab974d45c9f94ef270786ba4224b4c94c68cd26b
Luyss264 <joseacevedovargas@gmail.com>
2026-03-13 12:55:25 -0500

    Correcion Prints

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
-#PROGRAM IMPUT
+#=======================================ROGRAM IMPUT=======================================#
+
+sale = []
 from Sales_Calculation_Function import sales
+prosecute = "Y"
 
 print("Welcome, please enter the dsta to register the sale")
 
-try:
-    item = input("Enter article Name: ")
-    price = float(input("Enter Item Price:" ))
-    amount = int(input("Quantity of Items Sold: "))
+#==============================REGISTATION AND SALESS ORDER=================================#
 
-    if price ==float and amount ==int:
-        sales(price, amount)
+while prosecute.upper() != "N":    
+    try:
+        item = input("Enter article Name: ")
+        price = float(input("Enter Item Price:" ))
+        amount = int(input("Quantity of Items Sold: "))
         
-except:
-    print("Pleace enter numeric values in Price and Quality")
+        current_sale = {
+            "ARTICLE" : item,
+            "UNIT_PRICE" : price, 
+            "QUANTITY" : amount, 
+            "TOTAL" : price * amount  
+        }
+        sale.append(current_sale)
+
+        sales(price, amount)
+    
+    except ValueError: 
+        print("Pleace enter numeric values in Price and Quality")
+        
+    prosecute = input("Do you want to register another Sale? (Y/N): ").strip().upper()
+    
+    continue
+        
+#===================================SALES SUMMARY===================================#
+
+print("Sale Summary: ")
+print(f"{'ARTICLE'}{'UNIT PRICE'}{'QUANTITY'}{'TOTAL'}")
+
+total_sale = 0.0
+
+print("\n" + "="*70)
+print(f"{'ARTICLE':<25} {'UNIT_PRICE':>12} {'QUANTITY':>10} {'TOTAL':>12}")
+print("-"*70)
+
+for s in sale:
+    print(f"{s['ARTICLE']:<25} {s['UNIT_PRICE']:>12,.2f} {s['QUANTITY']:>10} {s['TOTAL']:>12,.2f}")
+    total_sale += s['TOTAL']
+
+print("-"*70)
+print(f"{'TOTAL OF THE SALE':<55}${total_sale:>12,.2f}")
+print("="*70)
